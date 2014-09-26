691b53742f4788f4c6267b2fc2f24396862c9bf8
xieyunzi <xieyunzi@foxmail.com>
2014-09-26 19:46:28 +0800

    webrick servlet

--- xyz-server/webrick_servlet.rb
+++ xyz-server/webrick_servlet.rb
@@ -1,8 +1,14 @@
 require 'webrick'
 
+class App
+  def call(env)
+    [200, {'Content-Type' => 'text/plain'}, "root path. now: #{Time.now}"]
+  end
+end
+
 class RootPath < WEBrick::HTTPServlet::AbstractServlet
-  def service req, res
-    status, headers, body = [200, {'Content-Type' => 'text/plain'}, "root path. now: #{Time.now}"]
+  def service(req, res)
+    status, headers, body = App.new.call(req.meta_vars)
 
     res.status = status
     headers.each { |k, v| res[k] = v }
